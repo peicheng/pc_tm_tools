14139496cb320f1fe95fce925463e6b45c19bd00
Peicheng Liao <peicheng5@gmail.com>
2013-04-03 15:00:08 +0800

    first commit for pc_tm_tools

---
+++ genTMGTDdate.py
@@ -0,0 +1,36 @@
+#coding=utf-8
+import os,sys
+"""
+    Gen TM GTD Date
+    ============
+    given month and gen GTD prifix
+
+    ex.
+    genTMGTDdate.py 1304 1 
+    return
+        * 130401 (一
+        * 130402 (二
+        ...
+"""
+#print sys.argv[0],sys.argv[1]
+chtweeklist=['一','二','三','四','五','六','日']
+if len(sys.argv) <3:
+    print """
+    python genTMGTDdate.py 1304 0
+                           monthprifix weekstartday
+    """
+    print 'len',len(sys.argv)
+    exit()
+prefixstr="* "+sys.argv[1]
+startchtweek=int(sys.argv[2])
+monthday=31
+for i in xrange(1,monthday+1):
+    #print i
+
+
+    if startchtweek==7:
+        startchtweek=0
+    print prefixstr+str(i).zfill(2)+' ('+chtweeklist[startchtweek]
+    startchtweek=startchtweek+1
+    #print startchtweek
+
