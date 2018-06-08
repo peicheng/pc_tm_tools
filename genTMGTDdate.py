647025f49dbd4f687b0427555ea043a644916821
PC Liao <peicheng5@gmail.com>
2018-06-08 16:30:09 +0800

    auto pep8

--- genTMGTDdate.py
+++ genTMGTDdate.py
@@ -1,5 +1,6 @@
-#coding=utf-8
-import os,sys
+# coding=utf-8
+import os
+import sys
 import datetime
 import calendar
 """
@@ -15,24 +16,25 @@ import calendar
         ...
 """
 #print sys.argv[0],sys.argv[1]
-chtweeklist=['一','二','三','四','五','六','日']
-if len(sys.argv) <2:
+chtweeklist = ['一', '二', '三', '四', '五', '六', '日']
+if len(sys.argv) < 2:
     print """
     python genTMGTDdate.py 1304 [0]
                            monthprifix weekstartday
     """
-    print 'len',len(sys.argv)
+    print 'len', len(sys.argv)
     exit()
-prefixstr="* "+sys.argv[1]
-startchtweek = int(datetime.datetime.strptime(sys.argv[1]+'01',"%y%m%d").weekday())
+prefixstr = "* "+sys.argv[1]
+startchtweek = int(datetime.datetime.strptime(
+    sys.argv[1]+'01', "%y%m%d").weekday())
 
-startchtweek, monthday = calendar.monthrange(int(sys.argv[1][:2]), int(sys.argv[1][2:]))
-for i in xrange(1,monthday+1):
+startchtweek, monthday = calendar.monthrange(
+    int(sys.argv[1][:2]), int(sys.argv[1][2:]))
+for i in xrange(1, monthday+1):
     #print i
 
-    if startchtweek==7:
-        startchtweek=0
+    if startchtweek == 7:
+        startchtweek = 0
     print prefixstr+str(i).zfill(2)+' ('+chtweeklist[startchtweek]
-    startchtweek=startchtweek+1
+    startchtweek = startchtweek + 1
     #print startchtweek
-
