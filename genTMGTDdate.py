a436361af0689e13f1b6918c8729bf05d7503013
PC Liao <peicheng@ITSBTW055-MAC.local>
2023-03-13 10:58:32 +0800

    add keep gtd mode

--- genTMGTDdate.py
+++ genTMGTDdate.py
@@ -18,11 +18,12 @@ import calendar
 #print sys.argv[0],sys.argv[1]
 chtweeklist = ['一', '二', '三', '四', '五', '六', '日']
 if len(sys.argv) < 2:
-    print """
+    print("""
     python genTMGTDdate.py 1304 [0] [+r|-r]
                            monthprifix weekstartday
-    """
-    print 'len', len(sys.argv)
+    python3 genTMGTDdate.py 2303 2 +r
+    """)
+    print('len', len(sys.argv))
     exit()
 reverse_flag = True
 if len(sys.argv) >= 3:
@@ -36,19 +37,22 @@ startchtweek = int(datetime.datetime.strptime(
 
 startchtweek, monthday = calendar.monthrange(
     int(sys.argv[1][:2]), int(sys.argv[1][2:]))
-
+if len(sys.argv) >= 3:
+    mode = sys.argv[2]
 L = []
-for i in xrange(1, monthday+1):
+for i in range(1, monthday+1):
     #print i
-
     if startchtweek == 7:
         startchtweek = 0
     # print prefixstr+str(i).zfill(2)+' ('+chtweeklist[startchtweek]
-    L.append(prefixstr+str(i).zfill(2)+' ('+chtweeklist[startchtweek])
+    if mode == '2':
+        L.append('GTD '+'{}.{}.'.format(prefixstr[2:4],prefixstr[4:6])+str(i).zfill(2)+' '+str(startchtweek+1))
+    else:
+        L.append(prefixstr+str(i).zfill(2)+' ('+chtweeklist[startchtweek]+str(startchtweek))
     startchtweek = startchtweek + 1
-    #print startchtweek
+    # print(startchtweek)
 if reverse_flag:
     L = reversed(L)
 
 for l in L:
-    print l
+    print(l)
